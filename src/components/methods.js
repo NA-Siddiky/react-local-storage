f06009e97523d8008011b2992f82862629514e3a
NA-Siddiky <siddiky.academic@gmail.com>
2021-09-23 01:59:00 +0600

    commit

--- src/components/methods.js
+++ src/components/methods.js
@@ -16,3 +16,24 @@ console.log(emptyObj)
 // delete emptyObj['a']
 // console.log(emptyObj);
 
+
+// let number = 5;
+
+let calculation = (number) => {
+    let result = 0;
+
+    for (let i = 1; i <= number; i++) {
+        // console.log(i);
+        if (i % 2 == 0) {
+            result = result + i
+        }
+        else {
+            result = result + (-i)
+        }
+    }
+    // console.log(result)
+    return result;
+}
+
+console.log(calculation(4))
+console.log(calculation(5))
